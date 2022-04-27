9bb87ad176d9136f3dc40fee8a054843a9dc1b24
prettyboycode <kimfilippa@gmx.de>
2022-04-27 21:09:47 +0200

    added location info

--- src/routes/location.svelte
+++ src/routes/location.svelte
@@ -1,1 +1,14 @@
 <p>Location</p>
+Please count in enough time for your arrival, so that we can all start together and time.
+
+As the pandemic situation is still around we ask you to take care, stay safe and do not attend if you feel certain symptoms.
+Please act according to current pandemic regulations in Germany/ Hamburg.
+
+University of fine Arts Hamburg
+Lerchenfeld 2
+22081 Hamburg
+
+U3 Mundsburg
+S1, S11 Landwehr
+U1 Wartenau
+Bus 25, 606, 172 Uferstraße
