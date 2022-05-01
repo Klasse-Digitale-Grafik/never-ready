4a0bc0deaf6530fd5abe62ba6b4c07ee7f11e154
prettyboycode <kimfilippa@gmx.de>
2022-05-01 09:46:21 +0200

    added ticket info

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -42,7 +42,11 @@
 <main>
 	<Text>
 		<Line {readable} delay={0}   href="/timetable" text={'[!0] Timetable'} />
-		<Line {readable} delay={100} href={ticketLink} text={'[!0] Tickets Please book your free ticket via the link. As there is limited space, ticketing is mandatory. We also kindly ask you to return your ticket in case you can not make it to the event. Thank You!'} />
+		<Line {readable} delay={100} href={ticketLink} text={'[!0] Tickets'} />
+		<Line {readable} delay={100} href={ticketLink} text={'[!0] Please book your free ticket via the link.'} />
+		<Line {readable} delay={100} href={ticketLink} text={'[!0] As there is limited space, ticketing is mandatory.'} />
+		<Line {readable} delay={100} href={ticketLink} text={'[!0] We also kindly ask you to return your ticket'} />
+		<Line {readable} delay={100} href={ticketLink} text={'[!0] in case you can not make it to the event. Thank You!'} />
 		<Line {readable} delay={200} href="/location" text={'[!0] Location'} />
 	</Text>
 </main>
