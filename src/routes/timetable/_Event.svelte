92b5155668ec1d4fcb19cd612e8f8f6292722aae
moritzebeling <git@moritzebeling.com>
2022-05-03 13:54:56 +0200

    link timetable events

--- src/routes/timetable/_Event.svelte
+++ src/routes/timetable/_Event.svelte
@@ -4,20 +4,22 @@
 
 </script>
 
-<li title="{event.day} {event.time}">
+<li>
+    <svelte:element this={event.link ? 'a' : 'div'} href={event.link} title="{event.day} {event.time}">
 
-    {#if event.title}
-        <h3>{event.title}</h3>
-    {/if}
+        {#if event.title}
+            <h3>{event.title}</h3>
+        {/if}
 
-    {#if event.subtitle}
-        <h4>{event.subtitle}</h4>
-    {/if}
+        {#if event.subtitle}
+            <h4>{event.subtitle}</h4>
+        {/if}
 
-    {#if event.location}
-        <h5>{event.location}</h5>
-    {/if}
+        {#if event.location}
+            <h5>{event.location}</h5>
+        {/if}
 
+    </svelte:element>
 </li>
 
 <style>  
