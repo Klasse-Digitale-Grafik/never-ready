18953a7c2f4c1bec7502f6cf04a33c1cf9a3a908
prettyboycode <kimfilippa@gmx.de>
2022-05-04 09:15:25 +0200

    synopsis text

--- src/routes/synopsis.svelte
+++ src/routes/synopsis.svelte
@@ -10,16 +10,19 @@
 
 <main>
 	<Text readable={true} text="
-		[!10] Ho[50]w c[50]an [!90]
-		[!20] we [50] [!80]
-		[!30] g[50]o [50] off [!70]
-		[!40] gr[50]id [50] an[50]d [!60]
-		[!50] br[50]eak [50] wi[50]th [!50]
-		[!60] our [50] [!40]
-		[!70] tem[50]plat[50]ed [!30]
-		[!80] rea[50]lity [!20]
-		[!90] ? [!10]
+		[!10] [10]From the[10] 9th to the 1[20]1th of June 2022 [!40]
+		[!0] we a[10]re organiz[30]ing a congress [!80]
+		[!70] [70] abou[30]t the visu[10]ality [!0]
+		[!30] of th[30]e Inte[10]rnet. [!10]
+		[!60] [40] Und[30]er the t[10]itle Never Ready [10] 
+		[50] it will ta[40]ke place a[10]t our art school, the [!50]
+		[60] HFB[20]K in Hambur[10]g, Germany.[!40]
+		[!60][!40]
 	" />
+
+	<p>
+		The multitude of spaces in the online world have widely been unified in the last few years.  Innovation and finding your own unique solutions have been pushed aside, easing us into lazy comfort. Together with designers, writers and artists from all over the world we want to go off grid for a breather and get out of our cozy pillows. Together we want to be nostalgic about a new web that has yet to be conjured by us and share new possibilities and viewpoints for a happier and more diverse time in front of and behind the screen. We envision a digital detox from the online anecdotes and how the internet should look and delve deep into new narrations that break up with our templated reality. To overthrow the golden rules of web design to which we are bound and embrace lost or new ways of how the web works.	
+	</p>
 </main>
 
 <style>
