af161fe22baf3ee70b5d944c5907f8ae9e6860ec
prettyboycode <kimfilippa@gmx.de>
2022-05-27 17:06:05 +0200

    Lutz Issler zu Sebastian Schmieg geändert speaker page content

---
+++ src/routes/speakers/sebastian-schmieg.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Text from '$lib/shuffledText/Text.svelte';
+</script>
+
+<svelte:head>
+    <title>Never Ready: Sebastian Schmieg</title>
+</svelte:head>
+
+<Text readable={true} text="
+[!0] [20] Sebast[30]ian Sc[10]hmieg (DE)[!50]
+" />
+
+<p>
+    Sebastian Schmieg investigates the algorithmic circulation of images, texts, and bodies. He creates playful interventions that penetrate the shiny surfaces of our networked society and explore the realities that lie behind them. In particular Schmieg focuses on labor, algorithmic management, and artificial intelligence. He works in a wide range of media including video, website, installation, artist book, custom software, lecture performance, and delivery service. Schmieg’s work has been exhibited internationally at The Photographers’ Gallery London, MdbK Leipzig, HeK Basel, and Chronus Art Center Shanghai. He is a professor of Interface Design at HTW Dresden and lives and works in Berlin and Dresden.
+</p>
+<nav class="links">
+    <a href="https://sebastianschmieg.com/">web</a>
+    <a href="https://twitter.com/s_schmieg">twitter</a>
+    <a href="https://www.instagram.com/sebastianschmieg/?hl=de">instagram</a>
+</nav>
+<style>
+    nav, p {
+        margin-top: 1em;
+    }
+</style>
