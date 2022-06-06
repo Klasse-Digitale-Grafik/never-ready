d3bd9b4104c920189808d73c1b178f6e6e88012e
moritzebeling <git@moritzebeling.com>
2022-06-06 10:49:19 +0200

    small text edits

--- src/routes/speakers/olia-lialina.svelte
+++ src/routes/speakers/olia-lialina.svelte
@@ -10,7 +10,7 @@
 [!10] [30] Oli[10]a Lial[30]ina (DE)[!10]
 " />
 
-<p>Olia Lialina (b. Moscow,1971) is among the best-known participants in the 1990s net.art scene - an early-days, network-based art pioneer. Her early work had a great impact on recognizing the Internet as a medium for artistic expression and storytelling. This century her continuous and close attention to Internet architecture, "net.language" and vernacular web has made her an important voice in contemporary art and new media theory. Lialina is credited with founding one of the earliest web galleries, Art Teleportacia. She is cofounder and keeper of One Terabyte of Kilobyte Age archive and a professor at Merz Akademie in Stuttgart, Germany and a GIF model.</p>
+<p>Olia Lialina (b. Moscow,1971) is among the best-known participants in the 1990s net.art scene – an early-days, network-based art pioneer. Her early work had a great impact on recognizing the Internet as a medium for artistic expression and storytelling. This century her continuous and close attention to Internet architecture, "net.language" and vernacular web has made her an important voice in contemporary art and new media theory. Lialina is credited with founding one of the earliest web galleries, Art Teleportacia. She is cofounder and keeper of One Terabyte of Kilobyte Age archive and a professor at Merz Akademie in Stuttgart, Germany and a GIF model.</p>
 
 <nav class="links">
     <a href="http://art.teleportacia.org/#CenterOfTheUniverse">web</a>
