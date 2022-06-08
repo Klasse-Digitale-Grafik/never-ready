80170083267b9a8518c59533ffc211135c16f764
moritzebeling <git@moritzebeling.com>
2022-06-08 17:12:05 +0200

    update value statement

--- src/routes/values-statement.svelte
+++ src/routes/values-statement.svelte
@@ -15,20 +15,51 @@
         Values Statement
     </h1>
 
-    <p>Hello everyone, the Klasse Digitale Grafik and the HFBK Hamburg are proud to finally welcome you all to the Never Ready Congress, and would like to thank all of our presenters and participants for taking the time to be here for this event. We kindly ask all our presenters and participants to carry an open and curious attitude to all the unique and interesting people, ideas, and practices that you will see throughout this congress. Our intention in bringing all of you together is to create a context for dialog between perspectives from the design world that are as diverse in experience as possible into a space without hierarchy to discuss and learn about the visuality of the internet and how our design practices could look by sharing thoughts and experiences and listening to each other.</p>
-
-    <p>A hierarchy free design space like this, where we have so many people at lots of different stages of their practices is a rare find, so everyone is heavily encouraged to step out of their comfort zones and try to meet, speak with, or have lunch with new and unexpected people from outside of their design spaces in the next three days during this congress.</p>
-
-    <p>As for the rhetoric of this congress, derogatory remarks towards anyone or discrimination based on age, disability, gender, sexual identity, ethnic origin, and/or religion, including racism, antisemitism, islamophobia, antiziganism, sexism or unwelcome sexual attention, homophobia, queerphobia, transphobia, ableism, ageism, classism, body shaming, lookism or any other discriminative language will not be tolerated here. Everyone is encouraged to practice attentiveness to each other’s boundaries and needs.</p>
-
-    <p>While we have this space, let’s think about how we want the design space to change in the future. The economic positioning of the designer offers some freedoms, but also has fewer safety nets for the people in this industry, and although this might not be an issue for those with financial privilege, this limits which perspectives are present in the design sphere to the privileged few. That really illustrates the importance of being self-aware and reflecting on one’s privileges as a designer. Political and economic solidarity between designers themselves and between designers and other workers makes design practice more ethical and sustainable and makes the designs being made better.</p>
+    <p>Hello everyone,<br />
+    the Klasse Digitale Grafik and the HFBK Hamburg are proud to welcome everyone here to the Never Ready Congress, 
+    and we would like to thank all of our presenters and participants for taking the time to be here for this event. </p>
     
-    <p>The act of designing isn’t just an individual’s accomplishment but a practice requiring collaboration and external input. Design work today is very competitive and the structure of the industry is extremely isolating, pushing an individualistic perspective on what design practice is supposed to be and giving way to exploitation. Soulless work done just to survive is the economic reality of many designers. Open-source knowledge, tools, and resources are a great example of the power of collaboration and are the thing holding up any one individual's design practice. Open source and shared resources help not only designers but also many other parts of modern society. Contributing to and Protecting these resources from privatization should be one of the main concerns of designers today. That is also why during this congress everyone is encouraged to share as much as possible and and take advantage of this opportunity to listen to what is being shared.</p>
-
-    <p>Currently there is a hierarchy in the types of work available to designers, but ideally all of this design work should be considered equally important. When considering what your practice could look like, it is good to consider how the different types of design work fit into your practice, such as visual, political, commercial, etc. without labeling any aspect as being more or less important than another. What are the different aspects of design practice and how are the gaps between these different aspects bridged to form a practice that is able to adapt to different circumstances, political or economic conditions and needs, and includes experimentation, collaboration, and doesn’t tie the value of a life to capital productivity.</p>
-
-    <p>As discussions grow further in the coming days of this congress, please be mindful that this space was designated to be without any hierarchy between any people in this congress including participants, presenters, the class, and the team that put this congress together. Feel free to ask any questions and participate in dialog with everyone else here…</p>
-
+    <p>We kindly ask everyone here to keep an open and curious attitude towards all the unique and interesting people, ideas, and practices that we will see throughout this congress. 
+    Our intention in bringing everyone together is to create a context for dialogue between different perspectives from the design world that are as diverse in experience as possible. 
+    We are here in this space aiming to be without hierarchy, to address the broad topic of the visuality of the Internet as well as our role and practice as designers in this field by sharing thoughts, experiences, and listening to each other.</p>
+    
+    <p>It is rare to have so many people at many different stages of their practices in a space like this where we intend to be free of hierarchy, 
+    so everyone is heavily encouraged to connect with others and try to meet, speak with, or have lunch with new and unexpected people from outside of their usual design spaces in the next three days during this congress. </p>
+    
+    <p>To create an atmosphere that feels kind and open to everyone, we need to be conscious of how we speak with and about each other. 
+    Therefore derogatory remarks towards anyone or discrimination based on age, disability, gender, sexual identity, ethnic origin, and/or religion, including racism, antisemitism, islamophobia, antiziganism, sexism or unwelcome sexual attention, homophobia, queerphobia, transphobia, ableism, ageism, classism, body shaming, or any other discriminative language will not be tolerated here. 
+    Everyone is encouraged to practice attentiveness to each other’s boundaries and needs.</p>
+    
+    <p>While we have this congress, let’s think about how we want design spaces, visualities, structures, and practices to change in the future. 
+    The economic positioning of the designer offers some freedoms, but also has fewer safety nets for the people in this industry. 
+    Although this might not be an issue for those with financial privilege, this limits the voices that are heard in design discourse to the privileged few. 
+    Let’s make self-awareness and reflecting on one’s privileges as a designer an essential part of any practice and discussion. 
+    Political and economic solidarity between designers themselves and between designers and other workers creates a more ethical and sustainable discourse, practice, and outcome.</p>
+    
+    <p>The act of designing isn’t just an individual’s accomplishment but a practice requiring collaboration and external input. 
+    Design work today is very competitive and the structure of the industry is extremely isolating, pushing an individualistic perspective on what design practice is supposed to be and giving way to exploitation. 
+    Soulless work done just to survive is the economic reality of many designers. Open-source knowledge, tools, and resources are a great example of the power of collaboration and are the things holding up any one individual's design practice. 
+    Opening up the static structures of authorship and ownership through open source or shared resources does not only concern designers but also computer scientists, politicians, and many more. 
+    Contributing to and protecting these resources from privatization should be one of the main concerns of designers today. 
+    That is also why, during this congress, everyone is encouraged to share as much as possible in the Never Ready group chat, guest book, and live discourse, and take advantage of this opportunity to listen and see what is being shared.</p>
+    
+    <p>Currently, there is a hierarchy in the types of work available to designers, but ideally, all of this design work should be considered equally important. 
+    As we contemplate what design practice could look like, let’s consider how the different types of design work fit into a design practice, such as visual, political, commercial, etc., without labeling any aspect as being more or less important than another. 
+    Let’s consider and discuss how the gaps between the different aspects of design can be bridged to form a practice that is adaptable to different social, political, or economic conditions, while still including experimentation, collaboration, and doesn’t tie the value of a life to economic productivity.</p>
+    
+    <p>As design students, we demand more from design education. 
+    The goal of art and design education is to prepare us for our future as artists and designers, which requires education structures to be able to teach from the height of present knowledge and technology. 
+    Education institutions must abandon the worshiping of one individual as an all-knowing master of their practice and shift focus to the goals and needs of the students they serve through community building and encouraging collaboration both within and between any given department or departments. 
+    Our education needs to teach us how to analyze criticize form, content, and context, while helping us build our visual literacy, and also provide the knowledge and tools to navigate the legal and financial structures and standards required to be able to have a practice that allows us to not only survive but also grow.</p>
+    
+    <p>The presence of international students, people of color, disabled, neurodivergent, or queer people should not be used as a gimmick to market a university or a class to outsiders for performative inclusivity. 
+    These students and designers are not passive decorations to a design setting, but rather need to be respected as any other vital part of any design space. 
+    They contribute a unique lens from which they view the world and the design practice, and it’s in the best interest of professors, administrators, managers, colleagues, and peers to take the time to hear and understand these perspectives, even if in doing so it requires questioning or unlearning out-dated or limiting design thinking. 
+    Many of the concepts and techniques we use stem from western thinking, but there are lots of pathways outside this mode of thought that are also worth taking the time to learn, understand, explore, and then experiment with through collaboration and critical reflection. </p>
+    
+    <p>We demand to work under inclusive and reliable structures where there is no need for design to isolate designers from one another and from other industries. 
+    Design must become more of a communal practice, where we all still have time to learn and grow and can be proud of our work. 
+    We are people first and are not defined by our industry or the structures above us. We work for a hierarchy-free design world, fair payment, fair treatment, community, and inclusivity.</p>
 
 </main>
 
